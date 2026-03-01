9fbfa5e21e555a94916e03a5b165a73133684fce
Tejas Mali <malitejas28@gmail.com>
2026-03-01 16:18:19 +0530

    fix

--- alarm-ui/api/alarm.js
+++ alarm-ui/api/alarm.js
@@ -1,10 +1,14 @@
-let alarm = { hour:"07", minute:"30", ampm:"AM" };
+let alarm = {
+  hour: "07",
+  minute: "30",
+  ampm: "AM"
+};
 
-export default function handler(req,res){
+export default function handler(req, res) {
 
-  if(req.method==="POST"){
-    alarm=req.body;
-    return res.status(200).json({ok:true});
+  if (req.method === "POST") {
+    alarm = req.body;
+    return res.status(200).json({ ok: true });
   }
 
   res.status(200).json(alarm);
